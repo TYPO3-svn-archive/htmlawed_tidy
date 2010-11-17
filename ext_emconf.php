8cc02f469a0deb1cd0563e10fb2cc62133c227c4
mousse <mousse@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-11-17 10:33:57 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/htmlawed_tidy/trunk@40324 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "htmlawed_tidy".
 #
-# Auto generated 16-11-2010 16:01
+# Auto generated 17-11-2010 11:33
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -23,9 +23,9 @@ $EM_CONF[$_EXTKEY] = array(
 more infos on http://forge.typo3.org/projects/show/extension-htmlawed_tidy',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '0.0.8',
+	'version' => '0.0.10',
 	'dependencies' => '',
-	'conflicts' => 'tstidy,abcsstidy,stfl_tidystdwrap',
+	'conflicts' => '0,1,2',
 	'priority' => '',
 	'loadOrder' => '',
 	'module' => '',
@@ -43,12 +43,15 @@ more infos on http://forge.typo3.org/projects/show/extension-htmlawed_tidy',
 	'constraints' => array(
 		'depends' => array(
 		),
-		'conflicts' => array('tstidy','abcsstidy','stfl_tidystdwrap'
+		'conflicts' => array(
+			'0' => 'tstidy',
+			'1' => 'abcsstidy',
+			'2' => 'stfl_tidystdwrap',
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:8:{s:9:"ChangeLog";s:4:"3612";s:23:"class.user_tidyHTML.php";s:4:"27fd";s:21:"ext_conf_template.txt";s:4:"4cd9";s:12:"ext_icon.gif";s:4:"8af6";s:17:"ext_localconf.php";s:4:"26ef";s:19:"doc/wizard_form.dat";s:4:"bc42";s:20:"doc/wizard_form.html";s:4:"e224";s:16:"pi1/htmLawed.php";s:4:"32c5";}',
+	'_md5_values_when_last_written' => 'a:10:{s:9:"ChangeLog";s:4:"3612";s:23:"class.user_tidyHTML.php";s:4:"4a98";s:21:"ext_conf_template.txt";s:4:"5732";s:12:"ext_icon.gif";s:4:"8af6";s:17:"ext_localconf.php";s:4:"53e5";s:25:"config/default_preset.cfg";s:4:"07bd";s:21:"config/xml_preset.cfg";s:4:"e403";s:19:"doc/wizard_form.dat";s:4:"bc42";s:20:"doc/wizard_form.html";s:4:"e224";s:16:"pi1/htmLawed.php";s:4:"32c5";}',
 	'suggests' => array(
 	),
 );
