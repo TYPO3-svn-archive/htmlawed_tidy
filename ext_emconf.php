e9e23f8c0dc186e67be9736346462f9229ef7ee3
mousse <mousse@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-11-17 15:42:10 +0000

    bug fix (spaces in options selection)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/htmlawed_tidy/trunk@40355 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "htmlawed_tidy".
 #
-# Auto generated 17-11-2010 16:25
+# Auto generated 17-11-2010 16:41
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -23,13 +23,13 @@ $EM_CONF[$_EXTKEY] = array(
 more infos on http://forge.typo3.org/projects/show/extension-htmlawed_tidy',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '0.0.12',
+	'version' => '0.1.1',
 	'dependencies' => '',
 	'conflicts' => '0,1,2',
 	'priority' => '',
 	'loadOrder' => '',
 	'module' => '',
-	'state' => 'testing',
+	'state' => 'beta',
 	'uploadfolder' => 0,
 	'createDirs' => '',
 	'modify_tables' => '',
@@ -51,7 +51,7 @@ more infos on http://forge.typo3.org/projects/show/extension-htmlawed_tidy',
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"3612";s:23:"class.user_tidyHTML.php";s:4:"8663";s:21:"ext_conf_template.txt";s:4:"e795";s:12:"ext_icon.gif";s:4:"8af6";s:17:"ext_localconf.php";s:4:"53e5";s:34:"config/htmlawed_default_preset.cfg";s:4:"b33f";s:30:"config/tidy_default_preset.cfg";s:4:"07bd";s:26:"config/tidy_xml_preset.cfg";s:4:"e403";s:19:"doc/wizard_form.dat";s:4:"bc42";s:20:"doc/wizard_form.html";s:4:"e224";s:16:"pi1/htmLawed.php";s:4:"32c5";}',
+	'_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"3612";s:23:"class.user_tidyHTML.php";s:4:"8663";s:21:"ext_conf_template.txt";s:4:"3144";s:12:"ext_icon.gif";s:4:"8af6";s:17:"ext_localconf.php";s:4:"53e5";s:34:"config/htmlawed_default_preset.cfg";s:4:"b33f";s:30:"config/tidy_default_preset.cfg";s:4:"07bd";s:26:"config/tidy_xml_preset.cfg";s:4:"e403";s:19:"doc/wizard_form.dat";s:4:"bc42";s:20:"doc/wizard_form.html";s:4:"e224";s:16:"pi1/htmLawed.php";s:4:"32c5";}',
 	'suggests' => array(
 	),
 );
